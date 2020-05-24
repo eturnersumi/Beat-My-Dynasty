50b349cf4350ebb9f00ffb8999cc28cb83515275
Impactful Actions <eturner.et3@outlook.com>
2020-05-23 18:36:37 -0700

    picture widget and placeholder added

--- client/src/components/Profiler/index.js
+++ client/src/components/Profiler/index.js
@@ -1,11 +1,40 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import placeholder from "../../images/basketball_image.jpg";
+import "./style.css"
 
-function Profiler() {
+function Profiler(props) {
 
+  
+    const photo = props.profilePhotoURL ? props.profilePhotoURL : placeholder;
+  const url = props.profilePhotoURL ? props.profilePhotoURL : "";
+  
 
   return(
     <div className="profileWrapper">
-      
+    <Container>
+    <Row>
+      <Col>
+        <Button variant="success" size="lg" onClick={props.showWidget}>
+          Upload picture
+        </Button>
+        <br />
+
+        <img className="profileImg" src={photo} />
+        <br />
+
+        <p>
+          This is the picture URL:{" "}
+          <span>
+            <a href={url}>{url}</a>
+          </span>
+        </p>
+      </Col>
+    </Row>
+  </Container>
     </div>
   )
 }
