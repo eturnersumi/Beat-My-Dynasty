8c45c66df8e810b9d94c02ca52c5ad82dbaef8cf
Impactful Actions <eturner.et3@outlook.com>
2020-05-31 17:00:19 -0700

    updating seed connection

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -1,7 +1,7 @@
 let mongoose = require("mongoose");
 let db = require("../models");
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/team", {
+mongoose.connect(process.env.MONGODB_URI ? process.env.MONGODB_URI : "mongodb://localhost/team", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
